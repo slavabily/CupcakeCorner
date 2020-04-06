7ae2e129f06d8c243ac3385679e5ca69e14d8076
slavabily <slavabily@me.com>
2020-04-06 17:22:52 +0300

    Initial Commit

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by slava bily on 5/4/20.
+//  Copyright © 2020 slava bily. All rights reserved.
+//
+
+import SwiftUI
+
+class User: ObservableObject, Codable {
+    
+    @Published var name = "Paul Hudson"
+    
+    enum CodingKyes: CodingKey {
+        case name
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKyes.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKyes.self)
+        try container.encode(name, forKey: .name)
+    }
+    
+    
+}
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+
+struct ContentView: View {
+    
+    @State private var results = [Result]()
+    
+    var body: some View {
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+    .onAppear(perform: loadData)
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                    DispatchQueue.main.async {
+                        self.results = decodedResponse.results
+                    }
+                    // if everything is good, so we can exit
+                    return
+                }
+            }
+            // if we're still here it means there was a problem
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+        }
+        .resume()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
