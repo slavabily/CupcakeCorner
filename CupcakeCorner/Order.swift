2520b76e119ff42452cb2209f6758687ab9a128b
slavabily <slavabily@me.com>
2020-04-07 22:49:54 +0300

    completion of local architecture of app

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -11,6 +11,29 @@ import SwiftUI
 class Order: ObservableObject {
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
     
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        return true
+    }
+    
+    var cost: Double {
+        // $2 per cake
+        var cost = Double(quantity) * 2
+        // complicated cakes cost more
+        cost += Double(type) / 2
+        // $1/cake for extra frosting
+        if extraFrosting {
+            cost += Double(quantity)
+        }
+        // $0.50/cake for sprinkles
+        if addSprinkles {
+            cost += Double(quantity) / 2
+        }
+        return cost
+    }
+    
     // cake detailes
     @Published var type = 0
     @Published var quantity = 3
